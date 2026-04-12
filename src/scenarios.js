62935b85abb104775ed819ace4780f1dc0a63fa6
Amir Poorfakhraei <a.poorfakhraei@gmail.com>
2026-04-12 16:58:51 -0500

    Adding New Scenarios

---
+++ src/scenarios.js
@@ -0,0 +1,55 @@
+export const scenarios = [
+  {
+    id: "grocery",
+    title: "Grocery Store",
+    prompt: `
+You are in a grocery store.
+
+Start naturally like a shop worker or helper.
+
+Examples:
+- Hello! What do you need today?
+- Are you looking for something?
+- I see you are checking the bread. Do you want help?
+
+Say ONE sentence and ask ONE simple question.
+`
+  },
+  {
+    id: "airport",
+    title: "Airport",
+    prompt: `
+You are at the airport.
+
+Start like staff or a traveler.
+
+Examples:
+- Hello! Where are you flying today?
+- Do you need help with your ticket?
+
+Say ONE sentence and ask ONE simple question.
+`
+  },
+  {
+    id: "bus",
+    title: "Bus",
+    prompt: `
+You are on a bus.
+
+Start like a passenger or driver.
+
+Examples:
+- Hi! Where are you going?
+- Is this your stop?
+
+Say ONE sentence and ask ONE simple question.
+`
+  },
+  {
+    id: "free",
+    title: "Free Chat",
+    prompt: null
+  }
+];
+
+// <a href="https://www.flaticon.com/free-icons/airport" title="airport icons">Airport icons created by Freepik - Flaticon</a>
